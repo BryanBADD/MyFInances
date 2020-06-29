4716c48386044f8fa220b1cd216f138658f4df2a
Bryan Butz <bryan.butz@gmail.com>
2020-06-29 10:44:35 -0400

    Remove .env variable from connection string

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //Create database connection
 // process.env.MONGODB_URI || process.env.REACT_APP_DBCONNECTION
 mongoose
-    .connect(process.env.MONGODB_URI || process.env.REACT_APP_DBCONNECTION,
+    .connect(process.env.MONGODB_URI,
         { useNewUrlParser: true, 
         useUnifiedTopology: true})
     .then(console.log("Connected to mongodb...")
