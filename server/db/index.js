b2efabee600e96936dc0db04550133af5d46edc8
Bryan Butz <bryan.butz@gmail.com>
2020-07-05 14:23:12 -0400

    Fix db connection string env variable

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //Create database connection
 // process.env.MONGODB_URI || process.env.REACT_APP_DBCONNECTION
 mongoose
-    .connect(process.env.MONGODB_URI,
+    .connect(process.env.MONGODB_URI || process.env.REACT_APP_DBCONNECTION,
         { useNewUrlParser: true, 
         useUnifiedTopology: true})
     .then(console.log("Connected to mongodb...")
