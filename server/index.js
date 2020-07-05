063ee180de530f6f47de0dcb46be7cbd88da439d
Bryan Butz <bryan.butz@gmail.com>
2020-07-05 15:28:22 -0400

    Change port

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
 })
 
 //Listen for port and confirm it is running
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
