f863859c1aa8b87ee83e08094a90d703dba9c790
Bryan Butz <bryan.butz@gmail.com>
2020-07-10 18:01:36 -0400

    Debug

--- src/components/cash-flow/cash-flow.js
+++ src/components/cash-flow/cash-flow.js
@@ -102,12 +102,12 @@ export default class Example extends PureComponent {
                     </div>
                     {data.map((cat, index) => {
                       const amount = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(parseFloat(cat.value));
-                      const fColor = "color:" + cat.color;
+                      const fColor = "color:" + COLORS[index];
                       console.log(fColor);
                       return (
                       <div className="row">
                         <div className="col-lg-6">
-                          <p>{cat.name}</p>
+                          <p style={{fColor}}>{cat.name}</p>
                         </div>
                         <div className="col-lg-6 right-align">
                           {amount}
